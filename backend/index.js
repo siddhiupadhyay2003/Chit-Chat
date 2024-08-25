87159a2293a9d3dbb4320963598a71f5ed4ed3f3
siddhi upadhyay <supadhyay_be21@thapar.edu>
2024-08-25 08:00:24 +0530

    frontend

--- backend/index.js
+++ backend/index.js
@@ -1,23 +1,18 @@
-const express = require("express");
-const cors = require("cors");
+import { useState } from "react";
 
-const app = express();
-app.use(express.json());
-app.use(cors({ origin: true }));
+import "./App.css";
 
-app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  try{
-      const r = await axios.put("https://api.chatengine.io/users/",
-        {username: username , secret: username , first_name : username},
-        {headers: {"private-key": "273ba699-a1b7-4a9c-9e4e-4da45de83158"}}
-      );
-      return res.status(r.status).json(r.data);
-  } 
-  catch (e) {
-    return res.status(e.response.status).json(e.response.data);
+import AuthPage from "./AuthPage";
+import ChatsPage from "./ChatsPage";
 
+function App() {
+  const [user, setUser] = useState(undefined);
+
+  if (!user) {
+    return <AuthPage onAuth={(user) => setUser(user)} />;
+  } else {
+    return <ChatsPage user={user} />;
   }
-});
+}
 
-app.listen(3001);
+export default App;
