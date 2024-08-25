96ddcf6be188ad125cb95cbf3447342d03011b23
siddhi upadhyay <supadhyay_be21@thapar.edu>
2024-08-25 09:43:32 +0530

    rest file running

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
-import { useState } from "react";
+const axios = require("axios");
+const express = require("express");
 
-import "./App.css";
+// Initialize Express app
+const app = express();
 
-import AuthPage from "./AuthPage";
-import ChatsPage from "./ChatsPage";
-
-function App() {
-  const [user, setUser] = useState(undefined);
-
-  if (!user) {
-    return <AuthPage onAuth={(user) => setUser(user)} />;
-  } else {
-    return <ChatsPage user={user} />;
+// Middleware to parse JSON bodies
+app.use(express.json());
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  // Get or create user on Chat Engine!
+  try {
+    const r = await axios.put(
+      "https://api.chatengine.io/users/",
+      { username: username, secret: username, first_name: username },
+      { headers: { "Private-Key": "a6c3a28f-c27b-49a9-b5e2-d3a504083d5d" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
   }
-}
+});
 
-export default App;
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
